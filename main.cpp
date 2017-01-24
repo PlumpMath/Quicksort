0b8395e9b0c191958184ba5bc54514e8f0399862
mindoo <leon2@sautour.net>
2017-01-24 21:04:52 +0100

    Initial commit + added the basic function structure for the program

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/*
+AUTHOR: Mindoodoo
+DESCRIPTION: Program that utilizes a quicksort algorithm in order to sort an integer array
+*/
+
+#include <iostream>
+
+#define SIZE 10
+
+//Prototypes
+int partition(int *array[SIZE]);
+void segmentBefore(int *array[SIZE], int beginning, int end, int pivot);
+void segmentAfter(int *array[SIZE], int beginning, int end, int pivot);
+
+/*
+INPUT: None
+DESCRIPTION: main function
+OUTPUT: 0
+*/
+int main() {
+
+}
+
+/*
+INPUT: Pointer to an array
+DESCRIPTION: finds a pivot point for the array
+OUTPUT: (int) Position of the pivot Pointer
+*/
+int partition(int *array[SIZE]) {
+
+}
+
+/*
+INPUT: Pointer to an array
+DESCRIPTION: Sorts the first segment of the given segment
+OUTPUT: None
+*/
+void segmentBefore(int *array[SIZE], int beginning, int end, int pivot) {
+
+}
+
+/*
+INPUT: Pointer to an array
+DESCRIPTION: Sorts the second segment of a given segment
+OUTPUT: None
+*/
+void segmentAfter(int *array[SIZE], int beginning, int end, int pivot) {
+
+}
